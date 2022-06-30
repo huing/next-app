3fb8c869560e7a71380a6d7b074a78fbf99f8893
huing <747541102@qq.com>
2022-06-30 20:44:47 +0800

    time

--- components/date.js
+++ components/date.js
@@ -1,6 +1,11 @@
-import { parseISO, format } from "date-fns";
+// import { parseISO, format, isValid } from "date-fns";
 
 export default function Date({ dateString }) {
-  const date = parseISO(dateString);
-  return <time dateTime={dateString}>{format(date, "LLLL d, yyyy")}</time>;
+  // console.log(isValid(new Date(dateString)));
+  return <time dateTime={dateString}>{dateString}</time>;
+  // if (!isValid(dateString)) {
+  //   return <time dateTime={"2020-10-10"}>2020-10-10</time>;
+  // }
+  // const date = parseISO(dateString);
+  // return <time dateTime={dateString}>{format(date, "LLLL d, yyyy")}</time>;
 }
